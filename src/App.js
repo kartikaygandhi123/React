7c82e260aff5b0fbb666a6fdedfb0b54d7851c42
Kartikay Gandhi <kartikaygandhi123@gmail.com>
2022-04-08 17:18:22 +0530

    Adding React Files For First Time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+
+
+import React, {useState} from 'react';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
 
 function App() {
+
+
+
+const [mode, setmode] = useState('light');
+
+
+
+
+
+
+
+const togglemode=()=>{
+
+if (mode==='light'){
+  setmode('dark')
+  document.body.style.backgroundColor='grey';
+  document.title='TextUtilities- Dark Mode';
+
+//   setInterval(()=>{
+
+//   document.title='TextUtilities- Dark Mode';
+//   },2000);
+
+
+// setInterval(()=>{
+//   document.title='Share This Website With Friends'
+// },1500);
+
+}
+
+
+
+else{
+  setmode('light')
+  document.body.style.backgroundColor='white';
+  document.title='TextUtilities- Light Mode';
+}
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<Navbar title="TextUtilities"   aboutText="About" mode={mode} togglemode={togglemode}        />
+
+
+
+
+<div className="container my-3">
+<TextForm heading="Enter The Text To Analyze" mode={mode}/>
+
+{/* <About/> */}
+</div>
+  </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+
+
+
+export default function TextForm(props) {
+
+const handleUpCase=()=>{
+    // console.log("Clicked"+text);
+    let newText=text.toUpperCase();
+    setText(newText)
+}
+const handleLowCase=()=>{
+    // console.log("Clicked"+text);
+    let newText=text.toLowerCase();
+    setText(newText)
+}
+const handleOnChange=(event)=>{
+     // console.log("On Change");
+    setText(event.target.value);
+}
+const handleTextClear=(event)=>{
+     // console.log("On Change");
+    setText("");
+}
+
+const handleExtraSpaces=()=>{
+
+let newText= text.split(/[ ]+/);
+setText(newText.join(" "))
+
+}
+
+
+
+    const[text,setText]=useState('Enter Text Here');
+    return (
+    
+    <>
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+
+
+<div className="mb-3">
+  <h1>{props.heading}</h1>
+  <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color:props.mode==='dark'?'white':'black'}}  id="myBox" rows="8"></textarea>
+</div>
+<button className="btn btn-primary mx-2" onClick={handleUpCase}>Convert to uppercase</button>
+
+<button className="btn btn-primary mx-2" onClick={handleLowCase}>Convert to Lower case</button>
+
+<button className="btn btn-primary mx-2" onClick={handleTextClear}>Clear The Text</button>
+
+<button className="btn btn-primary my-2 mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+
+
+    </div>
+   
+    <div className="container my-2" style={{color: props.mode==='dark'?'white':'black'}}>
+
+<h2>Your Text Summary</h2>
+<p>{text.split(" ").length} words, {text.length} characters</p>
+<p>{0.008*text.split(" ").length} Minutes Read</p>
+    
+<h3>Preview Text</h3>
+<p>{text.length>0?text:"enter something to be previewed"}</p>
+</div>
+    </>
+
+
+  )
+}
